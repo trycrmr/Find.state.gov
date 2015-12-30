ed74cc03a9921306fc8b3c2a92239891343e0809
Michael Ramos <mdramos8@gmail.com>
2015-12-30 17:51:38 -0500

    setup geojson stuff

--- src/common/components/charts/Map.js
+++ src/common/components/charts/Map.js
@@ -1,7 +1,5 @@
 import React, { PropTypes, Component } from 'react';
 
-
-
 export default class MapChart extends Component {
 
     // this gets called initially 
@@ -18,19 +16,52 @@ export default class MapChart extends Component {
 
     render() {
         
-        if(this.state.geoObj === null || !this.state.geoObj) {
-           return <h1>Loading ...</h1>
+        if(process.env.BROWSER) {
+            var { GeoJson, Map, Marker, Popup, TileLayer } = require('react-leaflet');
+            if(!this.state.geoObj) {
+               return <h1>Loading ...</h1>
+            }
+            else {
+                // this.state.geoObj is now accessable
+                // temporarily just uses sovereignt
+
+                // TODO ALL FUNCTIONALITY HERE
+                
+                const position = [51.505, -0.09];
+                console.log(this.state.geoObj)
+
+                var geojsonFeature = {
+                    "type": "Feature",
+                    "properties": {
+                        "name": "Coors Field",
+                        "amenity": "Baseball Stadium",
+                        "popupContent": "This is where the Rockies play!"
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-104.99404, 39.75621]
+                    }
+                };
+
+                return(
+
+                    // <Map center={position} zoom={13}>
+                    //     <TileLayer
+                    //       url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    //       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    //     />
+                    //     <Marker position={position}>
+                    //       <Popup>
+                    //         <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                    //       </Popup>
+                    //     </Marker>
+                    //   </Map>
+                    <Map>
+                        <GeoJson data={geojsonFeature} />
+                    </Map>
+                ) 
+            }
         }
-        else 
-            // this.state.geoObj is now accessable
-            // temporarily just uses sovereignt
-
-            // TODO ALL FUNCTIONALITY HERE
-            
-            return(
-
-                <h1>Map HTML Here</h1>
-            ) 
     }
 }
 
