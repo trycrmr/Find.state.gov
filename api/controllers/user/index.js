aa49c7394e97c7fe38d9db94e3b64839390c59f6
Michael Ramos <mdramos8@gmail.com>
2016-01-07 14:51:17 -0500

    auth structure changes

--- api/controllers/user/index.js
+++ api/controllers/user/index.js
@@ -15,11 +15,21 @@ var salt = bcrypt.genSaltSync(10);
 
 // global key used for jwt signing
 var secretKey = uuid.v4();
+// create token method
+function createToken(user) {
+	var claims = {
+		userID: user.User_ID,
+		iss: 'http://find.state.gov'
+	}
+	var jwt = nJwt.create(claims,secretKey);
+	return jwt.compact();
+} 
 
 module.exports = function (router) {
 
 	/**
 	 * @GET localhost/user
+	 *
 	 */
     router.get('/', function (req, res) {
     	// TODO REMOVE THIS - only use for testing
@@ -28,8 +38,11 @@ module.exports = function (router) {
 
 	/**
 	 * @POST localhost/user/validateToken
+	 * 
 	 */
     router.post('/validateToken', function (req, res) {
+    	// response object
+    	var resBody = {}
     	// user has a token and sends to us for user access
     	// if token is valid it will contain a userID
     	// use the userid to query user data and send back to client
@@ -42,7 +55,7 @@ module.exports = function (router) {
   			} else{
     			// todo query userID here
     			
-    			var resBody = {}
+    			
     			res.json(resBody)
   			}
 		});
@@ -50,50 +63,56 @@ module.exports = function (router) {
 
 	/**
 	 * @POST localhost/user/validateLogin
+	 *
 	 */
     router.post('/validateLogin', function (req, res) {
+    	// response object
+    	var resBody = {};
     	// user has submitted login credentials
     	// verify the credentials, send back user ibject and token if valid
     	// if invalid send an invalid response
 
     	// Load hash from your password DB.
-    	var resBody = {};
     	
     	User.findOne({
 		  where: {Email: req.body.email}
 		}).then(function(user) {
-		  	var userData = user.dataValues	
-		  	// got the user, now compare passwords
-		  	bcrypt.compare(req.body.password, userData.Password, function(err, valid) {
-	    		if (err) {
-	    			// passwords dont match
-	    			resBody = {valid:false, message:'could not authenticate user'}
-    				res.json(resBody)
-	    		} else {
-	    			// todo create session token
-
-	    			// -- response --
-			  		res.json(resBody)
-			  		// -- response --
-	    		}
-			});
-		}).catch(function(err){
-			resBody = {valid:false, message:'could not authenticate user'}
-			// -- response --
-			res.json(resBody)
-			// -- response --
+			if (user != null) { 
+			  	var userData = user.dataValues	
+			  	// got the user, now compare passwords
+			  	bcrypt.compare(req.body.password, userData.Password, function(err, valid) {
+		    		if (err) {
+		    			// passwords dont match
+		    			resBody = {valid:false, message:'could not authenticate user'}
+	    				res.json(resBody)
+		    		} else {
+		    			// todo create session token
+		    			var token = createToken(user)
+		    			resBody = {valid:true, token: token, user: user}
+		    			// -- response --
+				  		res.json(resBody)
+				  		// -- response --
+		    		}
+				});
+			  } else {
+			  	resBody = {valid:false, message:'could not authenticate user'}
+				// -- response --
+				res.json(resBody)
+				// -- response --
+			  }
 		})
-
 	});
 
 
 	/**
 	 * @POST localhost/user/register
+	 *
 	 */
     router.post('/register', function (req, res) {
+    	// response object
     	var resBody = {};
-
     	// lets check and see if email already exists first
+    	// db will return null when not found
     	User.findOne({
 		  where: {Email: req.body.email}
 		}).then(function(exists){
@@ -102,24 +121,23 @@ module.exports = function (router) {
 				// generate a hash for the password
 	    		// we are using async so out processing doesnt get tied up
 				bcrypt.genSalt(10, function(err, salt) {
-	    		bcrypt.hash(req.body.password, salt, function(err, hash) {
-	        		var User = model.User.build({
-			  			Name: req.body.name,
-			  			Email: req.body.email,
-			  			Password: hash
-					})
-					User.save().then(function(data) {
-						var token = createToken(data.User_ID)
-						resBody = {valid:true, token: token}
-						// -- response --
-			  			res.json(resBody)
-			  			// -- response --
-					}).catch(function(err) {
-						console.log(err);
-					})
-			    });
-			});
-
+		    		bcrypt.hash(req.body.password, salt, function(err, hash) {
+		        		var User = model.User.build({
+				  			Name: req.body.name,
+				  			Email: req.body.email,
+				  			Password: hash
+						})
+						User.save().then(function(user) {
+							var token = createToken(user)
+							resBody = {valid:true, token: token, user: user}
+							// -- response --
+				  			res.json(resBody)
+				  			// -- response --
+						}).catch(function(err) {
+							console.log(err);
+						})
+				    });
+				});
 			} else {
 				// email already exists in db
 				resBody = {valid:false, message:'Email already exists'}
@@ -131,15 +149,4 @@ module.exports = function (router) {
 		});
     });
 
-    function createToken(userID) {
-    	var claims = {
-				userID: userID,
-				iss: 'http://find.state.gov',
-				role: 'admin'
-			}
-		var jwt = nJwt.create(claims,secretKey);
-		return jwt.compact();
-    } 
-
 };
-
